ee4de78ac4d094a8b450818ba6a5ecdfb340e932
jacod2007 <jacod2007@naver.com>
2021-04-12 23:55:46 +0900

    설정변경

--- app/src/main/java/com/example/pizzaneck/Setting.java
+++ app/src/main/java/com/example/pizzaneck/Setting.java
@@ -1,15 +1,28 @@
 package com.example.pizzaneck;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.VibrationEffect;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.content.Context;
 import android.view.MenuItem;
-
-
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Build;
+import android.os.Vibrator;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.view.View;
+import android.widget.RadioGroup;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -20,17 +33,62 @@ public class Setting extends AppCompatActivity {
     DrawerLayout drawerLayout;
     NavigationView navView;
     Toolbar toolbar;
+    LinearLayout layout;
+    private RadioGroup radioGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
         setToolbar();
+        radioGroup = findViewById(R.id.idRGroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.idRBLight:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+                    case R.id.idRBDark:
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                }
+            }
+        });
+
+
+        RadioButton button2 = (RadioButton) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+                if(Build.VERSION.SDK_INT >=26){
+                    vibrator.vibrate(VibrationEffect.createOneShot(1000,10));
+                }else{
+                    vibrator.vibrate(1000);
+                }
+            }
+        });
+
+        RadioButton button1 = (RadioButton) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(),uri);
+                ringtone.play();
+            }
+        });
     }
+
     /* 툴바 및 툴바기능 설정 함수.
      * onCreate에서 호출
      * 클래스 내 DrawerLayout drawerLayout; NavigationView navView; Toolbar toolbar; 선언 필요
      */
+
+
     protected void setToolbar(){
         //툴바 설정
         toolbar = findViewById(R.id.toolbar);
@@ -102,5 +160,7 @@ public class Setting extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+
+
 }
 
