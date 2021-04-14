3274841ab33a7a9bf972d4793ecba2e4e985dd97
jacod2007 <jacod2007@naver.com>
2021-04-14 15:22:31 +0900

    뒤로가기버튼생성

--- app/src/main/java/com/example/pizzaneck/alarm.java
+++ app/src/main/java/com/example/pizzaneck/alarm.java
@@ -1,8 +1,13 @@
 package com.example.pizzaneck;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class alarm extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class alarm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);    //기존 title 지우기
+        actionBar.setDisplayHomeAsUpEnabled(true);      //뒤로가기 버튼 생성. 이 버튼을 메뉴바 버튼으로 사용
     }
 }
