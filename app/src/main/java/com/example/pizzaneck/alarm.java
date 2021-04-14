62872f501208e8e04974c3991abbd6d3b29441b5
jacod2007 <jacod2007@naver.com>
2021-04-14 23:18:55 +0900

    뒤로가기

--- app/src/main/java/com/example/pizzaneck/alarm.java
+++ app/src/main/java/com/example/pizzaneck/alarm.java
@@ -1,15 +1,24 @@
 package com.example.pizzaneck;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class alarm extends AppCompatActivity {
+    RadioButton customBtt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,26 @@ public class alarm extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowTitleEnabled(false);    //기존 title 지우기
-        actionBar.setDisplayHomeAsUpEnabled(true);      //뒤로가기 버튼 생성. 이 버튼을 메뉴바 버튼으로 사용
+        actionBar.setDisplayHomeAsUpEnabled(true);//뒤로가기 버튼 생성.
+
+        RadioButton sound1 = (RadioButton) findViewById(R.id.sound1);
+        sound1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(), uri);
+                ringtone.play();
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item ){
+        switch(item.getItemId()){
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
