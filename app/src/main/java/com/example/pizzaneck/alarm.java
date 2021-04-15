d9fd3c42f8dd782c28148a9947fe07a9dd1755e5
jacod2007 <jacod2007@naver.com>
2021-04-15 22:00:14 +0900

    효과음 추가

--- app/src/main/java/com/example/pizzaneck/alarm.java
+++ app/src/main/java/com/example/pizzaneck/alarm.java
@@ -4,11 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -18,7 +24,6 @@ import android.widget.RadioButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class alarm extends AppCompatActivity {
-    RadioButton customBtt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +39,80 @@ public class alarm extends AppCompatActivity {
         sound1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-                Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(), uri);
-                ringtone.play();
+                MediaPlayer player = MediaPlayer.create(alarm.this, R.raw.alien);
+                player.start();
+            }
+        });
+
+        RadioButton sound2 = (RadioButton) findViewById(R.id.sound2);
+        sound2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer player = MediaPlayer.create(alarm.this, R.raw.beep_once);
+                player.start();
+            }
+        });
+
+        RadioButton sound3 = (RadioButton) findViewById(R.id.sound3);
+        sound3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer player = MediaPlayer.create(alarm.this, R.raw.chaos);
+                player.start();
+            }
+        });
+
+        RadioButton sound4 = (RadioButton) findViewById(R.id.sound4);
+        sound4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer player = MediaPlayer.create(alarm.this, R.raw.luna);
+                player.start();
+            }
+        });
+
+        RadioButton sound5 = (RadioButton) findViewById(R.id.sound5);
+        sound5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer player = MediaPlayer.create(alarm.this, R.raw.milky_way);
+                player.start();
+            }
+        });
+
+        RadioButton sound6 = (RadioButton) findViewById(R.id.sound6);
+        sound6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer player = MediaPlayer.create(alarm.this, R.raw.orion);
+                player.start();
+            }
+        });
+
+        RadioButton sound7 = (RadioButton) findViewById(R.id.sound7);
+        sound7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer player = MediaPlayer.create(alarm.this, R.raw.prism);
+                player.start();
+            }
+        });
+
+        RadioButton sound8 = (RadioButton) findViewById(R.id.sound8);
+        sound8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer player = MediaPlayer.create(alarm.this, R.raw.signal);
+                player.start();
+            }
+        });
+
+        RadioButton sound9 = (RadioButton) findViewById(R.id.sound9);
+        sound9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer player = MediaPlayer.create(alarm.this, R.raw.voyager);
+                player.start();
             }
         });
     }
